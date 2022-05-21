a783ce8dfd739ed7f81d6a8a9641e8acfdd2d1d5
Jenny Moon <jenny0moon@gmail.com>
2022-05-21 13:52:27 +0100

    Add characters-id paths

--- pages/characters/[id].tsx
+++ pages/characters/[id].tsx
@@ -1,5 +1,46 @@
-function CharacterPage(){
-return <div>Character Page</div>
+import { Character } from "../../types";
+import Image from 'next/image';
+import imageLoader from "../../imageLoader";
+
+function CharacterPage({character}: {
+  character: Character
+}){
+return <div>
+  <h1>{character.name}</h1>
+  <Image 
+  loader={imageLoader}
+  unoptimized
+  src={character.image}
+  alt={character.name}
+  width="200px"
+  height="200px"
+  
+  />
+  </div>
+}
+
+export async function getStaticPaths(){
+  const res = await fetch("https://rickandmortyapi.com/api/character");
+  const { results }: GetCharactersResults = await res.json();
+
+  return{
+    paths: results.map(() => {
+      return {params: {id: String(character.id)}}
+    })
+  }
+}
+
+export async function getStaticProps({ params }: { params: { id: string } }) {
+  const res = await fetch(
+    `https://rickandmortyapi.com/api/character/${params.id}`
+  );
+  const character = await res.json();
+
+  return {
+    props: {
+      character,
+    },
+  };
 }
 
 export default CharacterPage
